43f5ec6a350a654722625df1fcb30c967ae72eee
Kamil Rusniak <kamil.rusniak01@gmail.com>
2023-05-07 12:18:47 +0200

    add cors package

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const { PrismaClient } = require("@prisma/client");
+const express = require("express");
+const cors = require("cors");
 
 const prisma = new PrismaClient();
-
-const express = require("express");
 const app = express();
 
-app.get("/api", async (req, res) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+let corsOptions = {
+  origin: ["http://localhost:3000", "https://online-bookshelf.com"],
+};
 
+app.use(cors(corsOptions));
+
+app.get("/api", async (req, res) => {
   const books = await prisma.book.findMany();
 
   // const books = await prisma.$queryRaw`SELECT * FROM book`;
